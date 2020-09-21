fa92262a2f9a494a447d2b69dd90060bdcd83206
diksha-kalra <kalradiksha11@gmail.com>
2020-09-21 14:58:42 +0530

    new merge contents

--- demo.java
+++ demo.java
@@ -1,9 +1,6 @@
 public class hello{
 public static  void main (String [] args){
-<<<<<<< HEAD
 system.out.println("test file ");
-=======
 system.out.println("test file is here");
->>>>>>> test
 }
 }
